d66130e106d07ce0618c296a457f5c5a349596b8
Andreas Johlander <andreas@student251-109.eduroam.uu.se>
2014-10-16 14:51:26 +0200

    adds shifted time intervals for the MVA and some other fixes

--- c_4_v_timing_mva.m
+++ c_4_v_timing_mva.m
@@ -31,8 +31,8 @@ elseif   (nargin ==4) || (nargin == 5)
     b4 = x4;
     
 end
-    
-    tint = [b1(1,1),b1(end,1)];
+
+tint = [b1(1,1),b1(end,1)];
 if 1
     tintR = [tint(1)-60, tint(2)+60]; %Makes sure tintR is at least 2 min.
     irf.log('warning','Trying to read CAA files C?_CP_AUX_POSGSE_1M...')
@@ -49,128 +49,124 @@ rC2 = R.R2;
 rC3 = R.R3;
 rC4 = R.R4;
 
+
+%---------------Plot-------------------
+fC4 = irf_plot(4,'newfigure');
+set(gcf,'PaperUnits','centimeters')
+xSize = 15; ySize = 15;
+xLeft = (21-xSize)/2; yTop = (30-ySize)/2;
+set(gcf,'PaperPosition',[xLeft yTop xSize ySize])
+set(gcf,'Position',[10 10 xSize*50 ySize*50])
+set(gcf,'paperpositionmode','auto') % to get the same printing as on screen
+clear xLeft xSize sLeft ySize yTop
+
+h = zeros(1,4);
+h(1) = irf_panel('C1');
+h(2) = irf_panel('C2');
+h(3) = irf_panel('C3');
+h(4) = irf_panel('C4');
+hold(h(1))
+hold(h(2))
+hold(h(3))
+hold(h(4))
+
+%Bz
+plot(h(1),b1(:,1),b1(:,column))
+plot(h(2),b2(:,1),b2(:,column))
+plot(h(3),b3(:,1),b3(:,column))
+plot(h(4),b4(:,1),b4(:,column))
+
+%line at 0
+fLine = plot(h(1),tint,[0,0]);
+set(fLine,'Color',[.5 .5 .5])
+fLine = plot(h(2),tint,[0,0]);
+set(fLine,'Color',[.5 .5 .5])
+fLine = plot(h(3),tint,[0,0]);
+set(fLine,'Color',[.5 .5 .5])
+fLine = plot(h(4),tint,[0,0]);
+set(fLine,'Color',[.5 .5 .5])
+
+%Meta
+%ylim([maxB*-0.9,maxB*0.9])
+xlim(h(1),tint)
+xlim(h(2),tint)
+xlim(h(3),tint)
+xlim(h(4),tint)
+
+
+ystr = '';
+
+switch column
+    case 2
+        ystr = 'B_{x} [nT]';
+    case 3
+        ystr = 'B_{y} [nT]';
+    case 4
+        ystr = 'B_{z} [nT]';
+end
+
+ylabel(h(1),ystr,'FontSize',16)
+ylabel(h(2),ystr,'FontSize',16)
+ylabel(h(3),ystr,'FontSize',16)
+ylabel(h(4),ystr,'FontSize',16)
+
+irf_timeaxis(h)
+
+
+while true
+    figure(1)
+    [x,y] = ginput(2);
+    if (exist('a','var')==1)
+        delete(a)
+    end
+    clickedAx = gca;
+    nCluster = find(h == clickedAx);
     
-    %---------------Plot-------------------
-    fC4 = irf_plot(4,'newfigure');
-    set(gcf,'PaperUnits','centimeters')
-    xSize = 15; ySize = 15;
-    xLeft = (21-xSize)/2; yTop = (30-ySize)/2;
-    set(gcf,'PaperPosition',[xLeft yTop xSize ySize])
-    set(gcf,'Position',[10 10 xSize*50 ySize*50])
-    set(gcf,'paperpositionmode','auto') % to get the same printing as on screen
-    clear xLeft xSize sLeft ySize yTop
-    
-    h = zeros(1,4);
-    h(1) = irf_panel('C1');
-    h(2) = irf_panel('C2');
-    h(3) = irf_panel('C3');
-    h(4) = irf_panel('C4');
-    hold(h(1))
-    hold(h(2))
-    hold(h(3))
-    hold(h(4))
-    
-    %Bz
-    plot(h(1),b1(:,1),b1(:,column))
-    plot(h(2),b2(:,1),b2(:,column))
-    plot(h(3),b3(:,1),b3(:,column))
-    plot(h(4),b4(:,1),b4(:,column))
-    
-    %line at 0
-    fLine = plot(h(1),tint,[0,0]);
-    set(fLine,'Color',[.5 .5 .5])
-    fLine = plot(h(2),tint,[0,0]);
-    set(fLine,'Color',[.5 .5 .5])
-    fLine = plot(h(3),tint,[0,0]);
-    set(fLine,'Color',[.5 .5 .5])
-    fLine = plot(h(4),tint,[0,0]);
-    set(fLine,'Color',[.5 .5 .5])
-    
-    %Meta
-    %ylim([maxB*-0.9,maxB*0.9])
-    xlim(h(1),tint)
-    xlim(h(2),tint)
-    xlim(h(3),tint)
-    xlim(h(4),tint)
-    
-    
-    ystr = '';
-    
-    switch column
+    b = 0;
+    switch nCluster
+        case 1
+            b = b1;
         case 2
-            ystr = 'B_{x} [nT]';
+            b = b2;
         case 3
-            ystr = 'B_{y} [nT]';
+            b = b3;
         case 4
-            ystr = 'B_{z} [nT]';
+            b = b4;
     end
     
-    ylabel(h(1),ystr,'FontSize',16)
-    ylabel(h(2),ystr,'FontSize',16)
-    ylabel(h(3),ystr,'FontSize',16)
-    ylabel(h(4),ystr,'FontSize',16)
+    clickInd1 = find_closest_index(x(1),b(:,1));
+    clickInd2 = find_closest_index(x(2),b(:,1));
     
-    irf_timeaxis(h)
+    M = [clickInd1, clickInd2]
     
+    if(M(1)>M(2))
+        M = fliplr(M);
+    end
     
-    while true
-        figure(1)
-        [x,y] = ginput(2);
-        if (exist('a','var')==1)
-            delete(a)
-        end
-        clickedAx = gca;
-        nCluster = find(h == clickedAx);
-        
-        b = 0;
-        switch nCluster
-            case 1
-                b = b1;
-            case 2
-                b = b2;
-            case 3
-                b = b3;
-            case 4
-                b = b4;
-        end
-        
-        clickInd1 = find_closest_index(x(1),b(:,1));
-        clickInd2 = find_closest_index(x(2),b(:,1));
-        
-        %discInd = get_disc_ind(b,(clickInd1+clickInd2)/2);
-        %M = get_interval(b,discInd);
-        M = [clickInd1, clickInd2];
-        
-        absB = sqrt(sum(b(:,2:4)'.^2));
-        maxB = max(absB);
-        
-        a = zeros(1,4);
-        for i = 1:4
-            a(i) = area(h(i),[b(M(1,1),1) b(M(1,2),1)], [maxB maxB],-maxB, 'FaceColor', [0.5,1,0.5]);
-            set(a(i),'EdgeColor','none')
-        end
-        
-        
-        
-        M1 = M;
-        M2 = M;
-        M3 = M;
-        M4 = M;
-        
-        v = disc_timing(...
-            b1(M1(1):M1(2),:),...
-            b2(M2(1):M2(2),:),...
-            b3(M3(1):M3(2),:),...
-            b4(M4(1):M4(2),:),...
-            rC1,rC2,rC3,rC4,nCluster);
-        
-        
-        out = v;
-        
-        
-        
+    absB = sqrt(sum(b(:,2:4)'.^2));
+    maxB = max(absB);
+    
+    a = zeros(1,4);
+    for i = 1:4
+        a(i) = area(h(i),[b(M(1,1),1) b(M(1,2),1)], [maxB maxB],-maxB, 'FaceColor', [0.5,1,0.5]);
+        set(a(i),'EdgeColor','none')
     end
     
     
+    
+    M1 = M;
+    M2 = M;
+    M3 = M;
+    M4 = M;
+    
+    v = disc_timing(b1,b2,b3,b4,rC1,rC2,rC3,rC4,M,nCluster);
+        
+    
+    
+    out = v;
+    
+end
+
+
 end
 
--- disc_timing.m
+++ disc_timing.m
@@ -1,9 +1,8 @@
-function [V] = disc_timing(b1,b2,b3,b4,r1,r2,r3,r4,nCluster)
+function [V] = disc_timing(b1,b2,b3,b4,r1,r2,r3,r4,M,nCluster)
 %DISC_TIMING Summary of this function goes here
 %   Detailed explanation goes here
 
 
-coord = 2; %2=max, 3=inter, 4=min
 switch nCluster
     case 1
         b = b1;
@@ -15,58 +14,80 @@ switch nCluster
         b = b4;
 end
 
+M
+bCut = b(M(1):M(2),:);
+bCut1 = b1(M(1):M(2),:);
+bCut2 = b2(M(1):M(2),:);
+bCut3 = b3(M(1):M(2),:);
+bCut4 = b4(M(1):M(2),:);
 
-%[bLMN1,l1,v1] = irf_minvar(b1);
-[~,l,v_minvar] = irf_minvar(b);
-bLMN1 = xyz2lmn(b1,v_minvar);
-bLMN2 = xyz2lmn(b2,v_minvar);
-bLMN3 = xyz2lmn(b3,v_minvar);
-bLMN4 = xyz2lmn(b4,v_minvar);
+% Correlation function for velocity calculation
+[V,dV] = c_4_v_xcorr([bCut(1,1),bCut(end,1)],b1,b2,b3,b4,r1,r2,r3,r4);
+
+% Calculating time difference from velocity and position
+rInd = find_closest_index(bCut1(1,1),r1(:,1));
+
+R1 = r1(rInd,2:4);
+R2 = r2(rInd,2:4);
+R3 = r3(rInd,2:4);
+R4 = r4(rInd,2:4);
+
+switch nCluster
+    case 1
+        tDiff = [(R1-R1)*V'/(V*V'),(R2-R1)*V'/(V*V'),(R3-R1)*V'/(V*V'),(R4-R1)*V'/(V*V')];
+    case 2
+        tDiff = [(R1-R2)*V'/(V*V'),(R2-R2)*V'/(V*V'),(R3-R2)*V'/(V*V'),(R4-R2)*V'/(V*V')];
+    case 3
+        tDiff = [(R1-R3)*V'/(V*V'),(R2-R3)*V'/(V*V'),(R3-R3)*V'/(V*V'),(R4-R3)*V'/(V*V')];
+    case 4
+        tDiff = [(R1-R4)*V'/(V*V'),(R2-R4)*V'/(V*V'),(R3-R4)*V'/(V*V'),(R4-R4)*V'/(V*V')];
+end
+
+%
+%-------------MVA-------------
+
+%Time-shifting the four intervals
+M1 = [find_closest_index(bCut1(1,1)+tDiff(1),b1(:,1)),find_closest_index(bCut1(end,1)+tDiff(1),b1(:,1))];
+M2 = [find_closest_index(bCut2(1,1)+tDiff(2),b2(:,1)),find_closest_index(bCut2(end,1)+tDiff(2),b2(:,1))];
+M3 = [find_closest_index(bCut3(1,1)+tDiff(3),b3(:,1)),find_closest_index(bCut3(end,1)+tDiff(3),b3(:,1))];
+M4 = [find_closest_index(bCut4(1,1)+tDiff(4),b4(:,1)),find_closest_index(bCut4(end,1)+tDiff(4),b4(:,1))];
+
+% bShift is the magnetic field centered around the discontinuity for each
+% s/c
+bShift = b(M(1):M(2),:);
+bShift1 = b1(M1(1):M1(2),:);
+bShift2 = b2(M2(1):M2(2),:);
+bShift3 = b3(M3(1):M3(2),:);
+bShift4 = b4(M4(1):M4(2),:);
 
 
-% 
-% zInd1 = get_crossing(bLMN1,'mean',coord);
-% zInd2 = get_crossing(bLMN2,'mean',coord);
-% zInd3 = get_crossing(bLMN3,'mean',coord);
-% zInd4 = get_crossing(bLMN4,'mean',coord);
-% 
-% if(length(zInd1)>1)
-%     disp('Warning: more than one zero crossing')
-%     zInd1 = zInd1(1);
-% end
-% if(length(zInd2)>1)
-%     disp('Warning: more than one zero crossing')
-%     zInd2 = zInd2(1);
-% end
-% if(length(zInd3)>1)
-%     disp('Warning: more than one zero crossing')
-%     zInd3 = zInd3(1);
-% end
-% if(length(zInd4)>1)
-%     disp('Warning: more than one zero crossing')
-%     zInd4 = zInd4(1);
-% end
-% 
-% 
-% tD1 = bLMN1(zInd1,1);
-% tD2 = bLMN2(zInd2,1);
-% tD3 = bLMN3(zInd3,1);
-% tD4 = bLMN4(zInd4,1);
-% tVec = [tD1,tD2,tD3,tD4];
-% tDiff = tVec-tD1;
-% %tDiff = [tD1-tD1,tD2-tD1,tD3-tD1,tD4-tD1];
+
+
+% Minimum variance analysis. Possibly do nested?
+[~,l,v_minvar] = irf_minvar(bShift);
+[~,l1,v_minvar1] = irf_minvar(bShift1);
+[~,l2,v_minvar2] = irf_minvar(bShift2);
+[~,l3,v_minvar3] = irf_minvar(bShift3);
+[~,l4,v_minvar4] = irf_minvar(bShift4);
+
+%LMN coordinate system of the clicked s/c
+bLMN1 = xyz2lmn(bShift1,v_minvar);
+bLMN2 = xyz2lmn(bShift2,v_minvar);
+bLMN3 = xyz2lmn(bShift3,v_minvar);
+bLMN4 = xyz2lmn(bShift4,v_minvar);
+
+nTiming = V/norm(V);
+nMVA1 = v_minvar1(3,:);
+nMVA2 = v_minvar2(3,:);
+nMVA3 = v_minvar3(3,:);
+nMVA4 = v_minvar4(3,:);
+
+fArrow = figure;
+arrow([0,0,0],nTiming)
 
 
 
-[V,dV] = c_4_v_xcorr([b(1,1),b(end,1)],b1,b2,b3,b4,r1,r2,r3,r4);
-
-R1 = r1(1,2:4);
-R2 = r2(1,2:4);
-R3 = r3(1,2:4);
-R4 = r4(1,2:4);
-tDiff = [0,(R2-R1)*V'/(V*V'),(R3-R1)*V'/(V*V'),(R4-R1)*V'/(V*V')];
-
-fn = plot_disc(bLMN1,bLMN2,bLMN3,bLMN4,tDiff);
+plot_disc(bLMN1,bLMN2,bLMN3,bLMN4,tDiff);
 
 titstr = ['s/c ', num2str(nCluster), '     ', 'l2/l3 = ', num2str(l(2)/l(3)),...
     '   ', 'n_{mivar} = ' num2str(v_minvar(3,:)),'    n_{timing} = ' num2str(V/norm(V)) ];
