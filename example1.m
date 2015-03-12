06a7003a5df33f820c5ab9301be4459c81ffc8e9
ajohlander <andreas.johlander@gmail.com>
2015-03-12 11:41:19 +0100

    adds another event

--- example1.m
+++ example1.m
@@ -4,9 +4,12 @@ clear all
 % Magnetosheath
 %tint = [irf_time([2002 03 27 10 16 00]) irf_time([2002 03 27 10 17 30])];
 % Magnetopause
-tint = [irf_time([2002 02 20 13 21 00]) irf_time([2002 02 20 13 23 00])];
+%tint = [irf_time([2002 02 20 13 21 00]) irf_time([2002 02 20 13 23 00])];
 % Magnetotail
 %tint = [irf_time([2003 08 24 18 43 00]) irf_time([2003 08 24 18 44 30])];
+%Magnetopause2
+t = irf_time([2002 03 30 13 11 00]);
+tint = t+[0 180];
 
 %B-field
 caa_download(tint,'C?_CP_FGM_FULL');
@@ -19,13 +22,19 @@ gseMagC4 = irf_get_data('B_vec_xyz_gse__C4_CP_FGM_FULL','caa','mat');
 
 %Downloads position data.
 caa_download(tint,'C?_CP_AUX_POSGSE_1M')
-R = [];
-R.R1 = irf_get_data('sc_r_xyz_gse__C1_CP_AUX_POSGSE_1M','caa','mat');
-R.R2 = irf_get_data('sc_r_xyz_gse__C2_CP_AUX_POSGSE_1M','caa','mat');
-R.R3 = irf_get_data('sc_r_xyz_gse__C3_CP_AUX_POSGSE_1M','caa','mat');
-R.R4 = irf_get_data('sc_r_xyz_gse__C4_CP_AUX_POSGSE_1M','caa','mat');
+% R = [];
+% R.R1 = irf_get_data('sc_r_xyz_gse__C1_CP_AUX_POSGSE_1M','caa','mat');
+% R.R2 = irf_get_data('sc_r_xyz_gse__C2_CP_AUX_POSGSE_1M','caa','mat');
+% R.R3 = irf_get_data('sc_r_xyz_gse__C3_CP_AUX_POSGSE_1M','caa','mat');
+% R.R4 = irf_get_data('sc_r_xyz_gse__C4_CP_AUX_POSGSE_1M','caa','mat');
+
+%Alternative
+R1 = irf_get_data('sc_r_xyz_gse__C1_CP_AUX_POSGSE_1M','caa','mat');
+R2 = irf_get_data('sc_r_xyz_gse__C2_CP_AUX_POSGSE_1M','caa','mat');
+R3 = irf_get_data('sc_r_xyz_gse__C3_CP_AUX_POSGSE_1M','caa','mat');
+R4 = irf_get_data('sc_r_xyz_gse__C4_CP_AUX_POSGSE_1M','caa','mat');
 
 
-
-%calls c_4_timing_mva for the z-axis
-c_4_v_timing_mva('gseMagC?',R,4);
+%calls c_4_timing_mva
+column = 4; % z-component of magnetic field (2=x, 3=y, 4=z).
+c_4_v_timing_mva('gseMagC?','R?',column);
